c551deda2001da3f10d3257edf39f78571e236d5
KillerHyena <crunchvelvet72@gmail.com>
2025-02-10 02:05:33 +0530

    Initial commit

--- Practical File/experiment_2.2.py
+++ Practical File/experiment_2.2.py
@@ -1,6 +1,8 @@
 #Write a Python program to print the Fibonacci series using a for loop
-a , b = 0 , 1
-n = int(input("Enter the number of terms in the fibonacci series : "))
+a, b = 0, 1
+n = int(input("Enter the number of terms in the Fibonacci series: "))
+print(f"Fibonacci series with {n} terms is:", end=" ")
 for i in range(n):
-    print(a)
-    a , b = b , a + b
+    print(a, end=" ")
+    a, b = b, a + b
+print()
